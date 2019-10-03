689d432a20e75cd07577cf2e5a40e3092941dd8b
Ryan Sage <ryansage09@gmail.com>
2019-10-03 10:18:26 -0400

    updated settings to allow for heroku

--- pintecipe/settings.py
+++ pintecipe/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django-heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -131,3 +132,5 @@ REACT_APP_DIR = "client"
 STATICFILES_DIRS = [
     os.path.join(REACT_APP_DIR, 'build', 'static')
 ]
+
+django_heroku.settings(locals())
