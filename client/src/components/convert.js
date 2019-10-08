9417925227a15760e8b2415c9ce1f625a8eb655c
Ryan Sage <ryansage09@gmail.com>
2019-10-08 17:53:39 -0400

    testing searching for specific text

--- client/src/components/convert.js
+++ client/src/components/convert.js
@@ -23,12 +23,14 @@ class Convert extends Component {
     this.convertTextArea(this.state.convert)
 
   }
+
   convertTextArea = (text) => {
     const { names, output } = text
     //const { output } = text
     console.log(names)
     //console.log('names type ', typeof names)
     let lines = names.split(/\r?\n/);
+    //let alnu = names.split(/[0-9]?[a-zA-Z]/)
     //let output = []
     
     for (let i = 0; i < lines.length; i++) {
@@ -39,16 +41,18 @@ class Convert extends Component {
             }
             //output.push(obj)
         }
-    //console.log('output object type ', typeof output[0])
-    console.log(output)
-    //console.log(lines)
-    this.setState({convert: {names, output}})
-    this.state.convert.output.forEach(function (instruction){
-        var x = instruction
-        console.log(x)
-    })
+    //console.log(alnu)
+    // //console.log(lines)
+    // this.setState({convert: {names, output}})
+    // this.state.convert.output.forEach(function (instruction){
+    //     var x = instruction
+    //     console.log(x)
+    // })
 
   }
+
+
+
 
   render() {
     return (
@@ -65,33 +69,36 @@ class Convert extends Component {
 export default Convert
 
 
-//   conversionInput = (evnt) => {
-//     let newOutput = {...this.state};
-//     newOutput[evnt.target.name] = evnt.target.value;
-//     this.setState({convert: newOutput})
-// }
 
-// handleConvertSubmit = (evnt) => {
-//     evnt.preventDefault();
-//     this.convertLines(this.state.convert)
-// }
-// convertLines = (convert) => {
-//     var lines = convert.input.split(/\n/);
-//     //let test;
-//     var output = []
-//     // for (var i = 0; i < lines.length; i++) {
-//     //   // only push this line if it contains a non whitespace character.
-//     //   if (/\S/.test(lines[i])) {
-//     //     convert.outputText.push('{' + test.trim(lines[i]) + '}');
-//     //     convert.output.push(test.trim(lines[i]));
-//     //   }
-//     // }
+
+
+
+//   convertTextArea = (text) => {
+//     const { names, output } = text
+//     //const { output } = text
+//     console.log(names)
+//     //console.log('names type ', typeof names)
+//     let lines = names.split(/\r?\n/);
+//     //let output = []
+    
 //     for (let i = 0; i < lines.length; i++) {
-//         output.push('{'+ lines.trim(lines[i]) +'}')
-//     }
+//             const obj = {stepNum: i+1, stepDesc: lines[i].trim()}
+//             const trimmedLine = lines[i].trim()
+//             if (trimmedLine !== "") {
+//                 output.push(obj);
+//             }
+//             //output.push(obj)
+//         }
+//     //console.log('output object type ', typeof output[0])
 //     console.log(output)
-//     // this.setState({convert: convert})
-//     //console.log(output);
-//     //input.val('[' + outputText + ']');
-   
+//     //console.log(lines)
+//     this.setState({convert: {names, output}})
+//     this.state.convert.output.forEach(function (instruction){
+//         var x = instruction
+//         console.log(x)
+//     })
+
 //   }
+
+
+
