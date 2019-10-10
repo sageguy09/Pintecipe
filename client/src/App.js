f17b6b9d130635caaba7ee8058fae24c276c4152
Ryan Sage <ryansage09@gmail.com>
2019-10-10 00:00:39 -0400

    adding initial nav bar styling

--- client/src/App.js
+++ client/src/App.js
@@ -266,6 +266,52 @@ class App extends React.Component {
 
   render = () => (
   <div>
+
+<section class="hero is-dark is-small">
+  <div class="hero-head">
+    <nav class="navbar">
+      <div class="container">
+        <div class="navbar-brand">
+          <a class="navbar-item">
+            <h1 class="title"> Pintecipe</h1>
+          </a>
+          <span class="navbar-burger burger" data-target="navbarMenuHeroA">
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+        </div>
+        <div id="navbarMenuHeroA" class="navbar-menu">
+          <div class="navbar-end">
+            <a class="navbar-item is-active">
+              Home
+            </a>
+            <a class="navbar-item">
+              Add User
+            </a>
+            <a class="navbar-item">
+              Add Recipe
+            </a>
+            <span class="navbar-item">
+              <a class="button is-info is-inverted">
+                <span class="icon">
+                  <i class="fas fa-user"></i>
+                </span>
+                <span>Current User</span>
+              </a>
+            </span>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+
+  <div class="hero-body">
+    <div class="container has-text-centered">
+    </div>
+  </div>
+</section>
+
     <aside>
       {/* <h1>New User Form and List of All Users</h1> */}
        {/* <NewUserForm addNewUser={this.addNewUser}/> */}
@@ -275,8 +321,8 @@ class App extends React.Component {
     <article>
     {/* <Convert /> */}
     {/* <NewRecipeForm  /> */}
-    <ReviewRecipeForm /> 
-    {/* <UserHomePage currentUser={this.getCurrentUser()}/> */}
+    {/* <ReviewRecipeForm />  */}
+    <UserHomePage currentUser={this.getCurrentUser()}/>
     {/* <RecipeDetails />  */}
     {/* {userRecipeListing(this.getCurrentUser())} */}
     {/* {recipeDetails(this.getCurrentUser().recipes[0])} */}
--- client/src/components/UserHomepage.js
+++ client/src/components/UserHomepage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "bulma/css/bulma.css"
 class UserHomePage extends React.Component {
     state = {
       user: {...this.props.currentUser},
